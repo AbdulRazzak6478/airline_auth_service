c4c8cb06817321dec6c0a1c1e7e394a00274029a
Abdul Razzak <abdulrazzak74832@gmail.com>
2026-07-11 22:58:54 +0530

    auth service setup

---
+++ src/main/java/com/airline/auth/AuthApplication.java
@@ -0,0 +1,23 @@
+package com.airline.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+//		<dependency>
+//				<groupId>org.springframework.boot</groupId>
+//				<artifactId>spring-boot-starter-actuator</artifactId>
+//				</dependency>
+
+//		<dependency>
+//				<groupId>org.springdoc</groupId>
+//				<artifactId>springdoc-openapi-starter-webmvc-ui</artifactId>
+//				<version>2.8.9</version>
+//				</dependency>
+	}
+
+}
---
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,39 @@
+
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TYPE user_role AS ENUM ( 'ADMIN','SUPER_ADMIN','AIRLINE_MANAGER', 'STAFF', 'CUSTOMER');
+
+CREATE TYPE user_status AS ENUM ( 'ACTIVE', 'INACTIVE', 'SUSPENDED');
+
+CREATE TABLE users(
+
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    first_name VARCHAR(50) NOT NULL ,
+    last_name VARCHAR(50) NOT NULL ,
+    email VARCHAR(50) NOT NULL,
+    mobile_number VARCHAR(20) NOT NULL ,
+    role user_role NOT NULL ,
+    password VARCHAR(255) NOT NULL ,
+    status user_status NOT NULL DEFAULT 'ACTIVE',
+    email_verified BOOLEAN DEFAULT false,
+
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT uk_users_email UNIQUE (email),
+    CONSTRAINT uk_users_mobile_number UNIQUE (mobile_number),
+
+    CONSTRAINT chk_users_first_name
+        CHECK (length(trim(first_name)) > 0),
+
+    CONSTRAINT chk_users_last_name
+        CHECK (length(trim(last_name)) > 0),
+
+    CONSTRAINT chk_users_email
+        CHECK (length(trim(email)) > 0),
+
+    CONSTRAINT chk_users_mobile_number
+        CHECK (length(trim(mobile_number)) > 0)
+);
